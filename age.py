7e0e0be2449ca47d89108d9daf4fbebfa5b166d1
shrinihegde <shrinihegdeknsr97@gmail.com>
2019-01-12 16:55:11 +0530

    age of the person

---
+++ age.py
@@ -0,0 +1,7 @@
+def names():
+    name=str(input("enter your name: "))
+    age=eval(input("enter your age: "))
+    total=0
+    total=2019+100-age
+    print(f"hey {name} you'll turn 100years in {total}")
+names()
