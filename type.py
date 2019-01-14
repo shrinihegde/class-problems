b383edd296054dfa8aff2c356d9c984687e40dba
shrinihegde <shrinihegdeknsr97@gmail.com>
2019-01-14 10:40:45 +0530

    updated program

---
+++ type.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    try:
+        print(int(a)+int(b))
+    except:
+        print(a+b)
+a=input("enter the same type of data1 to be added: ")
+b=input("enter the same type of data2 to be added: ")
+add(a,b)
+    
